380ab849e6065fdfbe1c749ba7adf9b5e0302108
marco_antero <marco@fullsuite.ph>
2024-01-08 16:02:41 +0800

    added new component, modified sidebar elements

--- src/components/universal/DashBNumofLeavesToday.jsx
+++ src/components/universal/DashBNumofLeavesToday.jsx
@@ -23,15 +23,18 @@ const DashBNumofLeaveToday = () => {
   return (
     <>
       <div className="m-2 p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 flex flex-1 flex-col items-center justify-between text-center md:w-56 h-56">
-        <h1 className="text-xl font-semibold text-left w-full">
+        <h1 className="text-lg font-semibold text-center w-full">
           Out of office
         </h1>
+        <h1 className="text-lg font-normal italic text-center w-full">
+          Today
+        </h1>
 
-        <h1 className="my-1 text-7xl font-bold">{countLeave}</h1>
+        <h1 className="my-1 text-5xl font-bold">{countLeave}</h1>
 
         <div className="text-right w-full">
           <span className="text-[12px] text-gray-500 italic">
-            Today {moment().format("MMMM DD, YYYY")}
+            {moment().format("MMMM DD, YYYY")}
           </span>
         </div>
       </div>
