c153bbb32e95a20e979ab9207027b0abc135511c
matt <matt@fullsuite.ph>
2024-01-09 14:56:30 +0800

    created pagination for own pto

--- src/components/universal/DashBOwnPTO.jsx
+++ src/components/universal/DashBOwnPTO.jsx
@@ -176,7 +176,7 @@ const DashBOwnPTO = ({ link }) => {
           <div className="m-2 p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 flex flex-col justify-center align-middle">
             <h1 className="text-lg font-semibold mb-4 text-center">Your PTOs</h1>
 
-            <DataTable columns={columns} data={myLeaves} highlightOnHover />
+            <DataTable columns={columns} data={myLeaves} pagination highlightOnHover />
           </div>
         </>
       </>
