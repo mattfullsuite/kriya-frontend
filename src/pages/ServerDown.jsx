9f5108846882f5f9792d50985cf755a4f562cda5
marco_antero <marco@fullsuite.ph>
2024-01-10 15:03:54 +0800

    added serverdownpage

---
+++ src/pages/ServerDown.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ServerDown = () => {
+    return(
+        <>
+        <div className="flex object-center">
+        <h1 className="text-5xl text-center font-extrabold">SERVER DOWN</h1>
+        <h1 className="text-2xl text-center">Its not you, its the server :(</h1>
+        </div>
+
+        </>
+    )
+}
+
+export default ServerDown;
