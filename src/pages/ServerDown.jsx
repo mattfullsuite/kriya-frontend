0cff5ee4c6c5b97b32b50e333ac04c6e9e6493bb
marco_antero <marco@fullsuite.ph>
2024-01-11 13:34:48 +0800

    modified serverdown frontend

--- src/pages/ServerDown.jsx
+++ src/pages/ServerDown.jsx
@@ -3,26 +3,33 @@ import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 
 const ServerDown = () => {
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const navigate = useNavigate();
 
-    const BASE_URL = process.env.REACT_APP_BASE_URL; 
-    const navigate = useNavigate()
+  useEffect(() => {
+    Axios.get(BASE_URL + "/login")
+      .then((response) => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        navigate("/serverDown");
+      });
+  }, []);
 
-    useEffect(() => {
-
-        Axios.get(BASE_URL + "/login").then((response) => {
-          navigate("/login");
-        }).catch((err) => {
-          console.log(err) 
-          navigate("/serverDown")});
-    }, []);
-
-    return(
-        <>
-        <h1 className="text-5xl text-center font-extrabold">SERVER DOWN</h1>
-        <h1 className="text-2xl text-center">It's not you, its the server :(</h1>
-
-        </>
-    )
-}
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+          {" "}
+          <h1 className="text-5xl text-center font-extrabold">SERVER DOWN</h1>
+          <h1 className="text-2xl text-center italic mt-2">
+            It's not you, its the server :(
+          </h1>
+          <img className="h-80 mt-10" src="/svgs/server_down.svg" alt="" />
+          
+      </div>
+    </>
+  );
+};
 
 export default ServerDown;
