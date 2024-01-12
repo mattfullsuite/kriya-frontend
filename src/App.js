42d71d4bf87e7a53e526eff0d87af8f00f9df74f
Marvin Bautista <marvin@fullsuite.ph>
2024-01-12 13:10:59 +0800

    skeleton and validation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import './App.css';
-import Dashboard from "./pages/Dashboard.jsx"
-import AddCompany from "./pages/AddCompany.jsx"
-import Login from "./pages/Login.jsx"
-import PendingLeaves from "./pages/PendingLeaves.jsx"
-import FileLeave from "./pages/FileLeave.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./pages/Dashboard.jsx";
+import AddCompany from "./pages/AddCompany.jsx";
+import Login from "./pages/Login.jsx";
+import PendingLeaves from "./pages/PendingLeaves.jsx";
+import FileLeave from "./pages/FileLeave.jsx";
 
-import EmployeeProfile from "./pages/EmployeeProfile.jsx"
-import EmployeesList from "./pages/admin/EmployeesList.jsx"
+import EmployeeProfile from "./pages/EmployeeProfile.jsx";
+import EmployeesList from "./pages/admin/EmployeesList.jsx";
 // import AddEmployee from "./pages/admin/AddEmployee.jsx"
-import Announcements from "./pages/hr/Announcements.jsx"
-import AddAnnouncements from "./pages/hr/AddAnnouncements.jsx"
+import Announcements from "./pages/hr/Announcements.jsx";
+import AddAnnouncements from "./pages/hr/AddAnnouncements.jsx";
 import ClientUserProfile from "./pages/client/ClientUserProfile.jsx";
 import AddEmployee from "./pages/hr/AddEmployee.jsx";
 
-import UserLogs from "./pages/admin/UserLogs.jsx"
+import UserLogs from "./pages/admin/UserLogs.jsx";
 
-import AdminDashboard from "./pages/admin/AdminDashboard.jsx"
-import LeadDashboard from "./pages/leads/LeadDashboard.jsx"
-import HRDashboard from "./pages/hr/HRDashboard.jsx"
-import ClientDashboard from "./pages/client/ClientDashboard"
-import WidgetPending from "./pages/widgets/WidgetPending.jsx"
+import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import LeadDashboard from "./pages/leads/LeadDashboard.jsx";
+import HRDashboard from "./pages/hr/HRDashboard.jsx";
+import ClientDashboard from "./pages/client/ClientDashboard";
+import WidgetPending from "./pages/widgets/WidgetPending.jsx";
 import LeadPTORequest from "./pages/leads/LeadPTORequests";
 import HRProfile from "./pages/hr/HRProfile.jsx";
 import ManagerProfile from "./pages/leads/ManagerProfile.jsx";
@@ -43,73 +43,80 @@ import ForgotPassword from "./pages/ForgotPassword.jsx";
 import ResetPassword from "./pages/ResetPasword.jsx";
 import ServerDown from "./pages/ServerDown.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
+      <SkeletonTheme baseColor="#f2f2f2" highlightColor="#ffffff">
+        <BrowserRouter>
+          <Routes>
+            {/* General Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route
+              path="/reset-password/:user_key"
+              element={<ResetPassword />}
+            />
 
-          {/* General Routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword/>} />
-          <Route path="/reset-password/:user_key" element={<ResetPassword/>}/>
+            {/* Administrator Routes */}
+            <Route path="/userLogs" element={<UserLogs />} />
+            <Route path="/adminDashboard" element={<AdminDashboard />} />
 
-          {/* Administrator Routes */}
-          <Route path="/userLogs" element={<UserLogs />} /> 
-          <Route path="/adminDashboard" element={<AdminDashboard />} /> 
+            {/* HR Routes */}
+            <Route path="/hrDashboard" element={<HRDashboard />} />
+            <Route path="/announcements" element={<Announcements />} />
+            <Route path="/addAnnouncements" element={<AddAnnouncements />} />
+            <Route path="/addEmployee" element={<AddEmployee />} />
+            <Route path="/editEmployee/:emp_id" element={<EditEmployee />} />
+            <Route path="/HRDirectory" element={<HRDirectory />} />
+            <Route path="/HRManage" element={<HRManage />} />
 
-          {/* HR Routes */}
-          <Route path="/hrDashboard" element={<HRDashboard />} /> 
-          <Route path="/announcements" element={<Announcements />} />
-          <Route path="/addAnnouncements" element={<AddAnnouncements />} />
-          <Route path="/addEmployee" element={<AddEmployee />} />
-          <Route path="/editEmployee/:emp_id" element={<EditEmployee />} />
-          <Route path="/HRDirectory" element={<HRDirectory/>} />
-          <Route path="/HRManage" element={<HRManage/>}/>
+            {/* <Route path="/viewEmployee" element={<ViewEmployee />} /> */}
+            <Route path="/viewEmployee/:emp_id" element={<ViewEmployee />} />
+            <Route path="/addCompany" element={<AddCompany />} />
+            <Route path="/hrProfile" element={<HRProfile />} />
+            <Route path="/hrAnnouncements" element={<HRAnnouncement />} />
+            <Route path="/hrAttendance" element={<HRAttendance />} />
+            <Route path="/hrTraining" element={<HRTraining />} />
 
-          {/* <Route path="/viewEmployee" element={<ViewEmployee />} /> */}
-          <Route path="/viewEmployee/:emp_id" element={<ViewEmployee />} />
-          <Route path="/addCompany" element={<AddCompany />} />
-          <Route path="/hrProfile" element={<HRProfile />} />
-          <Route path="/hrAnnouncements" element={<HRAnnouncement />} />
-          <Route path="/hrAttendance" element={<HRAttendance />} />
-          <Route path="/hrTraining" element={<HRTraining />} />
+            {/* Employee Routes */}
+            <Route path="/employees" element={<EmployeesList />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/employeeProfile" element={<EmployeeProfile />} />
+            <Route path="/clientDashboard" element={<ClientDashboard />} />
+            <Route path="/showPendingLeaves" element={<PendingLeaves />} />
+            <Route path="/fileLeave" element={<FileLeave />} />
+            <Route path="/empProfile" element={<ClientUserProfile />} />
+            <Route
+              path="/clientDirectory"
+              element={<ClientEmployeeDirectory />}
+            />
+            <Route
+              path="/clientAnnouncements"
+              element={<ClientAnnouncement />}
+            />
+            <Route path="/clientAttendance" element={<ClientAttendance />} />
+            <Route path="/clientTraining" element={<ClientTraining />} />
 
-          {/* Employee Routes */}
-          <Route path="/employees" element={<EmployeesList />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/employeeProfile" element={<EmployeeProfile />} />
-          <Route path="/clientDashboard" element={<ClientDashboard />} />
-          <Route path="/showPendingLeaves" element={<PendingLeaves />} />
-          <Route path="/fileLeave" element={<FileLeave />} />
-          <Route path="/empProfile" element={<ClientUserProfile />} />
-          <Route path="/clientDirectory" element={<ClientEmployeeDirectory />} />
-          <Route path="/clientAnnouncements" element={<ClientAnnouncement />} />
-          <Route path="/clientAttendance" element={<ClientAttendance />} />
-          <Route path="/clientTraining" element={<ClientTraining/>} />
+            {/* Team Lead Routes */}
+            <Route path="/leadDashboard" element={<LeadDashboard />} />
+            <Route path="/widgetPending" element={<WidgetPending />} />
+            <Route path="/leadPTORequests" element={<LeadPTORequest />} />
+            <Route path="/manProfile" element={<ManagerProfile />} />
+            <Route path="/leadDirectory" element={<LeadDirectory />} />
+            <Route path="/leadAnnouncements" element={<LeadAnnouncements />} />
+            <Route path="leadAttendance" element={<LeadAttendance />} />
+            <Route path="leadTraining" element={<LeadTraining />} />
 
-          {/* Team Lead Routes */}
-          <Route path="/leadDashboard" element={<LeadDashboard />} /> 
-          <Route path="/widgetPending" element={<WidgetPending />} />
-          <Route path="/leadPTORequests" element={<LeadPTORequest />} />
-          <Route path="/manProfile" element={<ManagerProfile />} />
-          <Route path="/leadDirectory" element={<LeadDirectory />} />
-          <Route path="/leadAnnouncements" element={<LeadAnnouncements/>}/>
-          <Route path="leadAttendance" element={<LeadAttendance/>}  />
-          <Route path="leadTraining" element={<LeadTraining/>}  />
-
-          <Route path="/serverDown" element={<ServerDown/>}  />
-          <Route path="*" element={<NotFound/>} />
-
-
-        </Routes>
-      </BrowserRouter>
-
+            <Route path="/serverDown" element={<ServerDown />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </SkeletonTheme>
     </div>
-    
   );
 }
 
--- src/components/universal/DashBRemainingPTO.jsx
+++ src/components/universal/DashBRemainingPTO.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const DashBremainingPTO = () => {
   const [ptos, setPtos] = useState([]);
   const BASE_URL = process.env.REACT_APP_BASE_URL; //
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserPTO = async () => {
       try {
-        const res = await Axios.get(BASE_URL + "/getUserPTO");
-        setPtos(res.data);
+        await Axios.get(BASE_URL + "/getUserPTO").then((res) => {
+          setPtos(res.data);
+          setIsLoading(false);
+        });
       } catch (err) {
         console.log(err);
       }
@@ -21,33 +26,39 @@ const DashBremainingPTO = () => {
     <>
       {/* Number of PTOs */}
 
-      {ptos.map((pto) => (
+      {isLoading ? (
         <div className="m-2 p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 flex flex-1 flex-col items-center justify-between text-center md:w-56 h-56">
-                  {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-10 h-10"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                  />
-                  </svg> */}
+          <h1 className="text-xl font-semibold text-left w-full"><Skeleton width={60} /></h1>
 
-          <h1 className="text-xl font-semibold text-left w-full">PTO points</h1>
-
-          <h1 className="my-1 text-7xl font-bold">{pto.leave_balance}</h1>
+          <h1 className="text-xl font-semibold center w-full">
+            <Skeleton height={60} width={60} />
+          </h1>
 
           <div className="flex flex-col justify-end items-end w-full">
-            <span className="text-[11px] text-gray-500 italic">Available until</span>
-            <span className="text-[12px] text-gray-500 italic">March 25, 2023</span>
+            <Skeleton height={12} width={60} />
+            <Skeleton height={12} width={80} />
           </div>
-        </div>  
-      ))}
+        </div>
+      ) : (
+        ptos.map((pto) => (
+          <div className="m-2 p-4 border-2 border-gray-200 border-solid rounded-lg dark:border-gray-700 flex flex-1 flex-col items-center justify-between text-center md:w-56 h-56">
+            <h1 className="text-xl font-semibold text-left w-full">
+              PTO points
+            </h1>
+
+            <h1 className="my-1 text-7xl font-bold">{pto.leave_balance}</h1>
+
+            <div className="flex flex-col justify-end items-end w-full">
+              <span className="text-[11px] text-gray-500 italic">
+                Available until
+              </span>
+              <span className="text-[12px] text-gray-500 italic">
+                March 25, 2023
+              </span>
+            </div>
+          </div>
+        ))
+      )}
     </>
   );
 };
