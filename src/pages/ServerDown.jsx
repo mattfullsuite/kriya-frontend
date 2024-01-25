ba693b4f966bee424cd3666570165db795626146
matt <matt@fullsuite.ph>
2024-01-25 15:12:11 +0800

    implementd website down, change metadata TsekSuite

--- src/pages/ServerDown.jsx
+++ src/pages/ServerDown.jsx
@@ -22,8 +22,17 @@ const ServerDown = () => {
       <div className="flex flex-col justify-center items-center h-screen">
           {" "}
           <h1 className="text-5xl text-center font-extrabold">SERVER DOWN</h1>
+          
           <h1 className="text-2xl text-center italic mt-2">
-            It's not you, its the server :(
+            It's not you, it's us. :(
+          </h1>
+
+          <h1 className="text-2xl text-center italic mt-2">
+            Kindly wait while we rebuild the website.
+          </h1>
+          
+          <h1 className="text-sm text-center italic mt-2">
+            - FS Engineering Department
           </h1>
           <img className="h-80 mt-10" src="/svgs/server_down.svg" alt="" />
           
