7cdacb7433c263f38e79beaf15beb71ff535e642
Matt Wilfred Salvador <smattwilfred01@gmail.com>
2024-02-07 15:56:11 +0800

    created methods date_separated

--- src/components/universal/EmployeeListComponent.jsx
+++ src/components/universal/EmployeeListComponent.jsx
@@ -21,16 +21,32 @@ const EmployeeListComponent = () => {
   const [filter, setFilter] = useState([]);
   const [query, setQuery] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [deactivated, setDeactivated] = useState([])
   const BASE_URL = process.env.REACT_APP_BASE_URL; //
+  const [all, setAll] = useState([]);
+  const [probationary, setProbationary] = useState([]);
+  const [regular, setRegular] = useState([]);
+  const [parttime, setPartTime] = useState([]);
+
 
   useEffect(() => {
     const fetchAllEmployees = async () => {
       try {
         const res = await axios.get(BASE_URL + "/employeeslist");
+        const res2 = await axios.get(BASE_URL + "/deactivatedAccounts");
+        const res3 = await axios.get(BASE_URL + "/allEmployees");
+        const res4 = await axios.get(BASE_URL + "/regularEmployees");
+        const res5 = await axios.get(BASE_URL + "/probationaryEmployees");
+        const res6 = await axios.get(BASE_URL + "/parttimeEmployees");
+        setAll(res3.data);
+        setProbationary(res5.data);
+        setRegular(res4.data);
+        setPartTime(res6.data)
         setEmployees(res.data);
         setFilter(res);
         setRecords(res.data);
         setIsLoading(false);
+        setDeactivated(res2.data)
       } catch (err) {
         console.log(err);
       }
@@ -46,6 +62,41 @@ const EmployeeListComponent = () => {
     });
     setRecords(newData);
   }
+
+  function handleAll() {
+    setRecords(employees);
+  }
+
+  function handleRegular(){
+    setRecords(regular)
+  }
+
+  function handleProbationary(){
+    setRecords(probationary)
+  }
+  function handleParttime(){
+    setRecords(parttime)
+  }
+
+  function handleDeactivated() {
+    setRecords(deactivated);
+  }
+
+  function handleChange(val) {
+    if(val === "All") {
+      setRecords(all);
+    } else if (val === "Regular"){
+      setRecords(regular)
+    } else if (val === "Probationary"){
+      setRecords(probationary)
+    } else if (val === "Part-time"){
+      setRecords(parttime)
+    } else if (val === "Deactivated"){
+      setRecords(deactivated)
+    }
+    
+  }
+
 
   const columns = [
     {
@@ -183,11 +234,36 @@ const EmployeeListComponent = () => {
         <div>
           <div className="mb-5">
             <input
+              id = "filter-dd"
               type="text"
               className="input input-bordered w-full md:w-1/3"
               placeholder="Search"
               onChange={handleFilter}
             />
+
+            {/*  */}
+
+            {/* <input type="radio" name="radio-1" className="radio-xs" onClick={handleAll}/><span>All</span>
+            <input type="radio" name="radio-1" className="radio-xs" onClick={handleProbationary}/><span>Probationary</span>
+            <input type="radio" name="radio-1" className="radio-xs" onClick={handleRegular}/> <span>Regular</span>
+            <input type="radio" name="radio-1" className="radio-xs" onClick={handleParttime}/><span>Part Time</span>
+            <input type="radio" name="radio-1" className="radio-xs" onClick={handleDeactivated}/> <span>Deactivated</span> */}
+
+            <select 
+              className="select select-bordered w-full max-w-xs mx-2"
+              onChange={(e) => {handleChange(e.target.value)}}
+            >
+              <option disabled selected>Filter</option>
+              <option>All </option>
+              <option>Regular </option>
+              <option>Probationary </option>
+              <option>Part-time </option>
+              <option>Deactivated </option>
+            </select>
+            
+
+            
+
           </div>
 
           <DataTable
