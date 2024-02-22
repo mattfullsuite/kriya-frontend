6a9e3ee98fed6d83390857e7025ce3f4dd69826b
Matt Wilfred Salvador <smattwilfred01@gmail.com>
2024-02-22 17:34:20 +0800

    Update on PTO request

--- src/components/universal/DashBButtons.jsx
+++ src/components/universal/DashBButtons.jsx
@@ -480,8 +480,7 @@ const DashBButtons = () => {
                         " " +
                         appr.s_name +
                         " (" +
-                        appr.dept_name +
-                        ")"}
+                        appr.dept_name + ")"}
                     </option>
                   ))}
                 </select>
