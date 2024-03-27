c966fee821c3df4c7059d791008858c58f76f8aa
Marvin Bautista <marvin.directobautista@gmail.com>
2024-03-27 13:05:15 +0800

    Added My Time Off & Attendacne page

--- src/components/universal/Headings.jsx
+++ src/components/universal/Headings.jsx
@@ -4,7 +4,7 @@ const Headings = ({text}) => {
 
     return (
         <>
-            <h1 className="mx-2 text-3xl font-bold">{text}</h1>
+            <h1 className="mx-2 text-2xl font-bold text-[#363636]">{text}</h1>
         </>
     )
 }
