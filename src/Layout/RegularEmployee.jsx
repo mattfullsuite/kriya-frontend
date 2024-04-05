0ed3bac578b89a373cadeab3dabd11e5ad4f37ec
Matt Wilfred Salvador <smattwilfred01@gmail.com>
2024-04-05 08:25:32 +0800

    Revamp

--- src/Layout/RegularEmployee.jsx
+++ src/Layout/RegularEmployee.jsx
@@ -143,9 +143,7 @@ const RegularEmployee = () => {
                   </span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
               </div>
             </div>
 
@@ -212,9 +210,7 @@ const RegularEmployee = () => {
                   </span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
               </div>
             </div>
 
@@ -278,13 +274,11 @@ const RegularEmployee = () => {
                     <path d="M20 3H5C3.346 3 2 4.346 2 6v12c0 1.654 1.346 3 3 3h15c1.103 0 2-.897 2-2v-2h-8c-1.103 0-2-.897-2-2V9c0-1.103.897-2 2-2h8V5c0-1.103-.897-2-2-2z"></path>
                   </svg>
                   <span className="text-[#A9A9A9] text-[14px]">
-                    My Benefits Manage...
+                    My Benefits Management
                   </span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
               </div>
             </div>
 
@@ -303,9 +297,7 @@ const RegularEmployee = () => {
                   <span className="text-[#A9A9A9] text-[14px]">My Pulse</span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
               </div>
             </div>
 
@@ -326,9 +318,7 @@ const RegularEmployee = () => {
                   </span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
               </div>
             </div>
 
@@ -347,9 +337,7 @@ const RegularEmployee = () => {
                   <span className="text-[#A9A9A9] text-[14px]">Team Chart</span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
               </div>
             </div>
 
@@ -371,9 +359,8 @@ const RegularEmployee = () => {
                   </span>
                 </div>
 
-                <span className="text-[10px] font-semibold mr-3 bg-yellow-400 px-1 py-[2px] rounded-[5px] text-[#363636]">
-                  LOCKED
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 fill-[#A9A9A9] mr-3"><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg>
+
               </div>
             </div>
 
