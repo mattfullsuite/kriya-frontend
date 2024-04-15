4fa299a2703e5d12eb680dfd5d8c9cc8738fb4ff
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-04-15 14:46:34 +0800

    Ingration: Pay Items in Payroll Settings

--- src/components/payrollaccountant/PayrollSettings.jsx
+++ src/components/payrollaccountant/PayrollSettings.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-import PIAddForm from "./sub-components/AddForm.jsx";
-import PIEditForm from "./sub-components/EditForm.jsx";
+import PIAddForm from "./sub-components/pay-item/AddForm.jsx";
+import PIEditForm from "./sub-components/pay-item/EditForm.jsx";
 // import DropdownCompany from "../components/DropdownCompany.jsx";
 import Swal from "sweetalert2";
 // import NoRecordFound from "../components/NoRecordFound.jsx";
@@ -23,39 +23,28 @@ function PayrollSettings() {
   const [isEditFormOpen, setEditFormOpen] = useState(false);
 
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const res = await axios.get(BASE_URL + "/login");
-        const payitems_res = await axios.get(
-          BASE_URL + "/mp-getcompanypayitems"
-        );
-
-        setUserData(res.data);
-        setPayItemsData(payitems_res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
     fetchUserProfile();
+    fetchPayItems();
   }, []);
 
-  useEffect(() => {
-    if (!userData) {
-      // Redirect to the login page if there is no cookie
-      navigate("/login");
+  const fetchUserProfile = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/login");
+
+      setUserData(res.data);
+    } catch (err) {
+      console.log(err);
     }
-  }, []);
+  };
 
-  // Get token from userData cookie
-  //   const getToken = () => {
-  //     const userData = JSON.parse(Cookies.get("userData"));
-  //     return userData.token;
-  //   };
-
-  //   const companyChange = (selectedCompany) => {
-  //     setCompanyID(selectedCompany);
-  //     getPayItems(selectedCompany);
-  //   };
+  const fetchPayItems = async () => {
+    try {
+      const payitems_res = await axios.get(BASE_URL + "/mp-getPayItem");
+      setPayItemsData(payitems_res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   //   const getPayItems = async (value_ID) => {
   //     try {
@@ -80,60 +69,56 @@ function PayrollSettings() {
   //     setRowSelected(true);
   //   };
 
-  //   const toggleDelete = (rowID) => {
-  //     Swal.fire({
-  //       title: "Are you sure?",
-  //       text: "You won't be able to revert this!",
-  //       icon: "warning",
-  //       confirmButtonText: "Yes, delete it!",
-  //       confirmButtonColor: "#Cc0202",
-  //       showCancelButton: true,
-  //       cancelButtonText: "Cancel",
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         //delete pay item
-  //         deletePayItem(rowID);
-  //       }
-  //     });
-  //   };
+  const toggleDelete = (rowID) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      confirmButtonText: "Yes, delete it!",
+      confirmButtonColor: "#Cc0202",
+      showCancelButton: true,
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        //delete pay item
+        deletePayItem(rowID);
+      }
+    });
+  };
 
-  //   const deletePayItem = async (recordID) => {
-  //     let status = "";
-  //     let message = "";
+  const deletePayItem = async (recordID) => {
+    let status = "";
+    let message = "";
 
-  //     try {
-  //       const token = getToken();
-  //       response = await axios.delete(
-  //         `/pay-item/remove/${recordID}`
-  //       );
+    try {
+      response = await axios.delete(BASE_URL + `/mp-deletePayItem/${recordID}`);
+      if (response.status === 200) {
+        status = "success";
+        message = "Record deleted successfully!";
 
-  //       if (response.status === 200) {
-  //         getPayItems(companyID);
-  //         console.log("Record deleted successfully!");
-  //         status = "success";
-  //         message = "Record deleted successfully!";
-  //       } else {
-  //         console.error("Failed to delete record");
-  //         status = "error";
-  //         message = "Failed to delete record";
-  //       }
-  //     } catch (error) {
-  //       console.error("Error viewing accounts: ", error);
-  //       status = "error";
-  //       message = "Failed to delete record";
-  //     } finally {
-  //       showAlert(status, message);
-  //     }
-  //   };
+        fetchPayItems();
+      } else {
+        console.error("Failed to delete record");
+        status = "error";
+        message = "Failed to delete record";
+      }
+    } catch (error) {
+      console.error("Error viewing accounts: ", error);
+      status = "error";
+      message = "Failed to delete record";
+    } finally {
+      showAlert(status, message);
+    }
+  };
 
-  //   const showAlert = (status, title) => {
-  //     Swal.fire({
-  //       icon: status,
-  //       title: title,
-  //       showConfirmButton: false,
-  //       timer: 1500,
-  //     });
-  //   };
+  const showAlert = (status, title) => {
+    Swal.fire({
+      icon: status,
+      title: title,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
 
   return (
     <>
@@ -144,13 +129,13 @@ function PayrollSettings() {
 
         <div className="mt-5 p-3 w-full h-96 lg:w-1/2 lg:h-1/2 bg-white border-2 border-gray-200 border-solid rounded-lg overflow-x-auto">
           {/* {companyID && dataTable ? ( */}
-          <div className="flex flex-col sm:flex-row gap-2">
+          <div className="flex justify-between gap-2">
             <div className="flex w-32 h-12 justify-center items-center">
               <h1 className="text-2xl font-bold">Pay Items</h1>
             </div>
-            <div className="w-fit flex flex-row gap-2">
-              {/* Search Input */}
-              <label className="input input-bordered w-72 flex items-center gap-2">
+            {/* <div className="w-fit flex flex-row gap-2"> */}
+            {/* Search Input */}
+            {/* <label className="input input-bordered w-72 flex items-center gap-2">
                 <input type="text" className="grow w-36" placeholder="Search" />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -164,11 +149,11 @@ function PayrollSettings() {
                     clipRule="evenodd"
                   />
                 </svg>
-              </label>
-              {/* Add Button/Form */}
-              <div className="w-fit flex items-end md:items-start">
-                <PIAddForm></PIAddForm>
-              </div>
+              </label> */}
+            {/* Add Button/Form */}
+            {/* </div> */}
+            <div className="w-fit flex items-end ">
+              <PIAddForm fetchPayItems={() => fetchPayItems()}></PIAddForm>
             </div>
           </div>
 
@@ -190,9 +175,10 @@ function PayrollSettings() {
                       <PIEditForm
                         payItemID={row.pay_items_id}
                         payItemData={row}
+                        fetchPayItems={() => fetchPayItems()}
                       ></PIEditForm>
                       <button
-                        //   onClick={() => toggleDelete(row.id)}
+                        onClick={() => toggleDelete(row.pay_items_id)}
                         className="btn btn-sm btn-danger bg-[#Cc0202] shadow-md px-4 text-white hover:bg-[#f7f7f7] hover:text-[#426E80] w-20"
                       >
                         Delete
