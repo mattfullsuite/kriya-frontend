bb0d3219ddedafdfd4431f467a2131f48297bf7b
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-04-29 14:43:51 +0800

    Update: Directory
    Updated directory of MyPayslips.jsx.

--- src/pages/leads/ManagerProfile.jsx
+++ src/pages/leads/ManagerProfile.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ManagerSideBar from "../../components/manager/ManagerSideBar";
-import Profile from "../../components/universal/Profile";
+import Profile from "../universal/Profile";
 
 const ManagerProfile = () => {
-    return(
-        <>
-        <Profile />
-        </>
-    )
-}
+  return (
+    <>
+      <Profile />
+    </>
+  );
+};
 
 export default ManagerProfile;
