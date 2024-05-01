fcda649541c2825601a77384b7311affeaa7934b
marvin <marvin@fullsuite.ph>
2024-05-01 14:36:59 +0800

    Team Chart and modification of HR sidebar

--- src/Layout/HREmployee.jsx
+++ src/Layout/HREmployee.jsx
@@ -663,7 +663,7 @@ const HREmployee = () => {
               </div>
             </div>
 
-            {/* <NavLink to="/hr/upload-csv">
+            <NavLink to="/hr/upload-csv">
               {(isActive) => {
                 return isActive.isActive ? (
                   <div className="flex flex-row justify-start items-center gap-8">
@@ -707,7 +707,7 @@ const HREmployee = () => {
                   </div>
                 );
               }}
-            </NavLink> */}
+            </NavLink>
 
             {/* Chimera Tab */}
 
@@ -1153,9 +1153,9 @@ const HREmployee = () => {
               </div>
             </div>
 
-            {/* <div className="divider mx-5 my-0"></div> */}
+            <div className="divider mx-5 my-0"></div>
 
-            {/* <NavLink to="/hr/employees">
+            <NavLink to="/hr/employees">
               {(isActive) => {
                 return isActive.isActive ? (
                   <div className="flex flex-row justify-start items-center gap-8">
@@ -1393,8 +1393,8 @@ const HREmployee = () => {
                   </div>
                 );
               }}
-            </NavLink> */}
-            <div className="flex flex-row justify-start items-center gap-8 w-full">
+            </NavLink>
+            {/* <div className="flex flex-row justify-start items-center gap-8 w-full">
               <div className="invisible bg-none h-7 w-[6px] rounded-r-[8px]" />
 
               <div className="flex flex-row justify-between items-center w-full">
@@ -1402,7 +1402,7 @@ const HREmployee = () => {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
-                    className="w-6 h-6 fill-[#A9A9A9]"
+                    className="w-5 h-5 fill-[#A9A9A9]"
                   >
                     <path d="m2.344 15.271 2 3.46a1 1 0 0 0 1.366.365l1.396-.806c.58.457 1.221.832 1.895 1.112V21a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-1.598a8.094 8.094 0 0 0 1.895-1.112l1.396.806c.477.275 1.091.11 1.366-.365l2-3.46a1.004 1.004 0 0 0-.365-1.366l-1.372-.793a7.683 7.683 0 0 0-.002-2.224l1.372-.793c.476-.275.641-.89.365-1.366l-2-3.46a1 1 0 0 0-1.366-.365l-1.396.806A8.034 8.034 0 0 0 15 4.598V3a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v1.598A8.094 8.094 0 0 0 7.105 5.71L5.71 4.904a.999.999 0 0 0-1.366.365l-2 3.46a1.004 1.004 0 0 0 .365 1.366l1.372.793a7.683 7.683 0 0 0 0 2.224l-1.372.793c-.476.275-.641.89-.365 1.366zM12 8c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4z"></path>
                   </svg>
@@ -1417,7 +1417,7 @@ const HREmployee = () => {
                   <path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path>
                 </svg>
               </div>
-            </div>
+            </div> */}
           </div>
 
           <div className="box-border bg-white border-t border-[#e4e4e4] p-2 flex flex-row justify-between items-center w-full">
