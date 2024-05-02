64e5348ccd38ba914e7f42366c91a9932d697bbd
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-05-02 13:19:13 +0800

    Function: Dipslay and Search

--- src/pages/pay-run-management/pay-run-requests/PayRunRequests.jsx
+++ src/pages/pay-run-management/pay-run-requests/PayRunRequests.jsx
@@ -1,8 +1,87 @@
 import Headings from "../../../components/universal/Headings";
 import RequestsTable from "./components/RequestsTables";
+import figma from "../../../assets/figma.png";
 
 const PayRunRequests = () => {
-  const requestData = [];
+  const requestData = [
+    {
+      requestID: 1,
+      requesterPic: figma,
+      requesterName: "Requester 1",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 1",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Executed",
+    },
+    {
+      requestID: 2,
+      requesterPic: figma,
+      requesterName: "Requester 2",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 2",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Approved",
+    },
+    {
+      requestID: 3,
+      requesterPic: figma,
+      requesterName: "Requester 3",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 3",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Pending",
+    },
+    {
+      requestID: 4,
+      requesterPic: figma,
+      requesterName: "Requester 4",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 4",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Approved",
+    },
+    {
+      requestID: 5,
+      requesterPic: figma,
+      requesterName: "Requester 5",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 5",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Approved",
+    },
+    {
+      requestID: 6,
+      requesterPic: figma,
+      requesterName: "Requester 6",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 6",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Pending",
+    },
+    {
+      requestID: 7,
+      requesterPic: figma,
+      requesterName: "Requester 7",
+      requesterJT: "Job Title",
+      recipientName: "Recipient 7",
+      recipientJT: "Job Title",
+      requestType: "Request Type",
+      dateRequested: "MM/DD/YYY",
+      status: "Executed",
+    },
+  ];
   return (
     <>
       <div>
