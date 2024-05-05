9002c8c6707495c08119ff8d171ec246b6e44cf8
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-05-05 18:22:27 +0800

    Update: Data
    Change from static data to database data.

--- src/pages/pay-run-management/pay-run-reports/PayRunReports.jsx
+++ src/pages/pay-run-management/pay-run-reports/PayRunReports.jsx
@@ -1,51 +1,31 @@
-import { duration } from "moment";
+import { useEffect, useRef, useState } from "react";
 import Headings from "../../../components/universal/Headings";
 import ReportsTable from "./components/ReportsTable";
+import axios from "axios";
 
 const PayRunReports = () => {
-  let reportsData = [
-    {
-      id: 1,
-      dateTimeGenerated: "MM/DD/YY hh:mm:ss",
-      duration: "MM/DD/YY - MM/DD/YY",
-      payDate: "01/20/24",
-      source: "Uploaded",
-    },
-    {
-      id: 2,
-      dateTimeGenerated: "MM/DD/YY hh:mm:ss",
-      duration: "MM/DD/YY - MM/DD/YY",
-      payDate: "01/20/24",
-      source: "Created",
-    },
-    {
-      id: 3,
-      dateTimeGenerated: "MM/DD/YY hh:mm:ss",
-      duration: "MM/DD/YY - MM/DD/YY",
-      payDate: "02/05/24",
-      source: "Uploaded",
-    },
-    {
-      id: 4,
-      dateTimeGenerated: "MM/DD/YY hh:mm:ss",
-      duration: "MM/DD/YY - MM/DD/YY",
-      payDate: "02/05/24",
-      source: "Created",
-    },
-    {
-      id: 5,
-      dateTimeGenerated: "MM/DD/YY hh:mm:ss",
-      duration: "MM/DD/YY - MM/DD/YY",
-      payDate: "02/05/24",
-      source: "Uploaded",
-    },
-  ];
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const reportsData = useRef([]);
+
+  const fetchData = async () => {
+    try {
+      const result = await axios.get(BASE_URL + "/mp-getAllPaySlip");
+      console.log(result.data);
+      reportsData.current = result.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
         <Headings text={"Pay Run Reports"} />
-        <ReportsTable reportsData={reportsData} />
+        <ReportsTable reportsData={reportsData.current} />
       </div>
     </>
   );
