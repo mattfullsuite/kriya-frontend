4b394e29764b0ac1cb3165914a51e000f561956e
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-05-06 08:42:29 +0800

    Fix: Data

--- src/pages/pay-run-management/pay-run-reports/PayRunReports.jsx
+++ src/pages/pay-run-management/pay-run-reports/PayRunReports.jsx
@@ -4,28 +4,11 @@ import ReportsTable from "./components/ReportsTable";
 import axios from "axios";
 
 const PayRunReports = () => {
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  const reportsData = useRef([]);
-
-  const fetchData = async () => {
-    try {
-      const result = await axios.get(BASE_URL + "/mp-getAllPaySlip");
-      console.log(result.data);
-      reportsData.current = result.data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <>
       <div>
         <Headings text={"Pay Run Reports"} />
-        <ReportsTable reportsData={reportsData.current} />
+        <ReportsTable />
       </div>
     </>
   );
