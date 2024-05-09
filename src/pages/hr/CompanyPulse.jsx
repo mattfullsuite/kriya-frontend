6aeaefde05721830c8d77a5da087bb9b2701c6a1
marvin <marvin@fullsuite.ph>
2024-05-09 14:36:07 +0800

    Company Pulse

---
+++ src/pages/hr/CompanyPulse.jsx
@@ -0,0 +1,12 @@
+import Headings from "../../components/universal/Headings";
+
+
+const CompanyPulse = () => {
+    return(
+        <div className="box-border max-w-[1300px] m-auto">
+            <Headings text={"Company Pulse"} />
+        </div>
+    )
+}
+
+export default CompanyPulse;
