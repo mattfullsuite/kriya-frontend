4e9c20308b7d172d699b4c1fc1e0bb9a865d76cf
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-05-16 16:01:00 +0800

    Integrate: Upload Payrun

--- src/pages/pay-run-management/create-upload/tabs/LastPayrun.jsx
+++ src/pages/pay-run-management/create-upload/tabs/LastPayrun.jsx
@@ -1,143 +1,141 @@
 const LastPayrun = () => {
   return (
-    <div className="flex flex-col md:flex-row box-border gap-3 p-5">
-      <div className="w-1/3 card ">
-        <div className="p-4 card-body rounded-[15px]">
-          <table className="">
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">Employee Name:</p>
-              </td>
-              <td>
-                <input
-                  type="text"
-                  placeholder="Search Employee"
-                  className="input input-bordered input-sm w-full mt-4"
-                />
-              </td>
-            </tr>
+    <div className="mt-10 flex flex-col md:flex-row box-border gap-3 p-5">
+      <div className="p-2 w-1/3 card rounded-[15px]">
+        <table className="">
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">Employee Name:</p>
+            </td>
+            <td>
+              <input
+                type="text"
+                placeholder="Search Employee"
+                className="input input-bordered input-sm w-full mt-4"
+              />
+            </td>
+          </tr>
 
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">Employee ID:</p>
-              </td>
-              <td>
-                <input
-                  style={{
-                    border: "1px solid #e4e4e4",
-                    backgroundColor: "#f2f2f2",
-                  }}
-                  type="text"
-                  placeholder="Type here"
-                  className="input input-bordered input-sm w-full mt-4"
-                  disabled
-                />
-              </td>
-            </tr>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">Employee ID:</p>
+            </td>
+            <td>
+              <input
+                style={{
+                  border: "1px solid #e4e4e4",
+                  backgroundColor: "#f2f2f2",
+                }}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered input-sm w-full mt-4"
+                disabled
+              />
+            </td>
+          </tr>
 
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">Hire Date:</p>
-              </td>
-              <td>
-                <input
-                  style={{
-                    border: "1px solid #e4e4e4",
-                    backgroundColor: "#f2f2f2",
-                  }}
-                  type="date"
-                  className="input input-bordered input-sm w-full mt-4"
-                  disabled
-                />
-              </td>
-            </tr>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">Hire Date:</p>
+            </td>
+            <td>
+              <input
+                style={{
+                  border: "1px solid #e4e4e4",
+                  backgroundColor: "#f2f2f2",
+                }}
+                type="date"
+                className="input input-bordered input-sm w-full mt-4"
+                disabled
+              />
+            </td>
+          </tr>
 
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">End Date:</p>
-              </td>
-              <td>
-                <input
-                  type="date"
-                  className="input input-bordered input-sm w-full mt-4"
-                />
-              </td>
-            </tr>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">End Date:</p>
+            </td>
+            <td>
+              <input
+                type="date"
+                className="input input-bordered input-sm w-full mt-4"
+              />
+            </td>
+          </tr>
 
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">End Date</p>
-                <p className="text-right pr-4 text-xs">
-                  (13th month pay calculation)
-                </p>
-              </td>
-              <td>
-                <input
-                  type="date"
-                  className="input input-bordered input-sm w-full mt-4"
-                />
-              </td>
-            </tr>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">End Date</p>
+              <p className="text-right pr-4 text-xs">
+                (13th month pay calculation)
+              </p>
+            </td>
+            <td>
+              <input
+                type="date"
+                className="input input-bordered input-sm w-full mt-4"
+              />
+            </td>
+          </tr>
 
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">Base Pay:</p>
-              </td>
-              <td>
-                <input
-                  style={{
-                    border: "1px solid #e4e4e4",
-                    backgroundColor: "#f2f2f2",
-                  }}
-                  type="text"
-                  placeholder="Type here"
-                  className="input input-bordered input-sm w-full  mt-4"
-                  disabled
-                />
-              </td>
-            </tr>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">Base Pay:</p>
+            </td>
+            <td>
+              <input
+                style={{
+                  border: "1px solid #e4e4e4",
+                  backgroundColor: "#f2f2f2",
+                }}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered input-sm w-full  mt-4"
+                disabled
+              />
+            </td>
+          </tr>
 
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">Last Payrun:</p>
-              </td>
-              <td>
-                <input
-                  style={{
-                    border: "1px solid #e4e4e4",
-                    backgroundColor: "#f2f2f2",
-                  }}
-                  type="date"
-                  className="input input-bordered input-sm w-full mt-4"
-                  disabled
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <p className="mt-4 text-right pr-4">Pro-rated 13th</p>
-                <p className="text-right pr-4">Month Pay:</p>
-              </td>
-              <td>
-                <input
-                  style={{
-                    border: "1px solid #e4e4e4",
-                    backgroundColor: "#f2f2f2",
-                  }}
-                  type="text"
-                  placeholder="Type here"
-                  className="input input-bordered input-sm w-full mt-4"
-                  disabled
-                />
-              </td>
-            </tr>
-          </table>
-          <div className="card-actions justify-end mt-4">
-            <button className="btn bg-[#666A40] text-white">Populate</button>
-          </div>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">Last Payrun:</p>
+            </td>
+            <td>
+              <input
+                style={{
+                  border: "1px solid #e4e4e4",
+                  backgroundColor: "#f2f2f2",
+                }}
+                type="date"
+                className="input input-bordered input-sm w-full mt-4"
+                disabled
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <p className="mt-4 text-right pr-4">Pro-rated 13th</p>
+              <p className="text-right pr-4">Month Pay:</p>
+            </td>
+            <td>
+              <input
+                style={{
+                  border: "1px solid #e4e4e4",
+                  backgroundColor: "#f2f2f2",
+                }}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered input-sm w-full mt-4"
+                disabled
+              />
+            </td>
+          </tr>
+        </table>
+        <div className="card-actions justify-end mt-4">
+          <button className="btn bg-[#666A40] text-white">Populate</button>
         </div>
       </div>
-      <div className="w-2/3 overflow-x-auto">
+      <div className="p-2 w-2/3 overflow-x-auto">
         <table className="table">
           <thead>
             <tr className="bg-[#666A40] text-white">
