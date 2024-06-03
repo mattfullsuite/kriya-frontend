9bb2fccdb02d87c35782e30311e6ae5133f5e629
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-06-03 11:46:49 +0800

    Update: Initial Value

--- src/pages/pay-run-management/create-upload/tabs/upload-payrun/Index.jsx
+++ src/pages/pay-run-management/create-upload/tabs/upload-payrun/Index.jsx
@@ -10,6 +10,7 @@ import Headings from "../../../../../components/universal/Headings.jsx";
 import NoRecord from "../../../components/NoRecord.jsx";
 import PreviewDialog from "./PreviewDialog.jsx";
 import moment from "moment";
+import { addCommaAndFormatDecimal } from "../../../assets/addCommaAndFormatDecimal.js";
 
 const UploadPayrun = () => {
   const companyInfo = useRef({});
@@ -27,9 +28,33 @@ const UploadPayrun = () => {
   const [sendEnable, setSendEnable] = useState(false);
   // Base URL for Axios
   const BASE_URL = process.env.REACT_APP_BASE_URL;
-
+  const payslipInfoInitial = {
+    "Employee ID": "Employee ID",
+    "Last Name": "Last Name",
+    "First Name": "First Name",
+    "Middle Name": "Middle Name",
+    Email: "Email",
+    "Job Title": "Job Title",
+    "Hire Date": moment().format("YYYY-MM-DD"),
+    Dates: {
+      From: moment().format("YYYY-MM-DD"),
+      To: moment().format("YYYY-MM-DD"),
+      Payment: moment().format("YYYY-MM-DD"),
+    },
+    "Pay Items": {
+      Earnings: { payitem1: "1.0", payitem2: "2.0" },
+      Deductions: { payitem1: "-1.0", payitem2: "-2.0" },
+    },
+    Totals: { Earnings: 3.0, Deductions: -3.0 },
+    "Net Pay": 1000.0,
+    source: "Created",
+  };
   let rowData = {
-    Dates: { From: moment().date, To: "", Payment: "" },
+    Dates: {
+      From: moment().format("YYYY-MM-DD"),
+      To: moment().format("YYYY-MM-DD"),
+      Payment: moment().format("YYYY-MM-DD"),
+    },
     Email: "",
     "Employee ID": "",
     "First Name": "",
@@ -46,7 +71,7 @@ const UploadPayrun = () => {
     Payment: "",
   };
   const [Dates, setDates] = useState(dates);
-  const [selectedRow, setSelectedRow] = useState(rowData);
+  const [selectedRow, setSelectedRow] = useState(payslipInfoInitial);
 
   useEffect(() => {
     fetchUserProfile();
@@ -512,7 +537,124 @@ const UploadPayrun = () => {
             )}
           </div>
         </div>
+        {/* <dialog id="row-data" className="modal">
+          <div className="modal-box p-0 w-11/12 max-w-3xl">
+            <div className="flex flex-col px-5 py-5 bg-gradient-to-br  from-[#666A40] to-[#a0a47d]  text-white justify-end">
+              <div className="flex flex-row">
+                <button
+                  className="m-r ml-auto"
+                  onClick={() => document.getElementById("row-data").close()}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex flex-row justify-between mt-5">
+                <div className="w-full font-bold">
+                  {selectedRow["Employee ID"]}
+                </div>
+                <div className="w-full text-end">
+                  <span className="font-bold">Pay Period: </span>
+                  <span>{selectedRow.Dates["From"]}</span>
+                  <span className="font-bold"> to </span>
+                  <span>{selectedRow.Dates["To"]}</span>
+                </div>
+              </div>
+              <div className="flex flex-row justify-between mt-2">
+                <div className="w-full font-bold">
+                  {selectedRow["First Name"]} {selectedRow["Middle Name"]}{" "}
+                  {selectedRow["Last Name"]}
+                </div>
+                <div className="w-full text-end">
+                  <span className="font-bold">Pay Day: </span>
+                  {selectedRow.Dates["Payment"]}
+                </div>
+              </div>
+              <div className="flex flex-row justify-between mt-2">
+                <div className="w-full font-bold">
+                  {selectedRow["Job Title"]}
+                </div>
+                <div className="w-full text-end"></div>
+              </div>
+            </div>
+            <div className="flex flex-row px-5 pb-5">
+              <div className="flex flex-col lg:flex-row w-full">
+                <div className="w-full">
+                  <h1 className="font-bold mx-3 mt-3">Pay Calculation</h1>
+                  <hr className="mt-1 border h-[5px] bg-[#000000]"></hr>
+                  {Object.entries(selectedRow["Pay Items"]).map(
+                    ([category, payItems]) => (
+                      <>
+                        <div
+                          className="flex flex-row justify-between"
+                          key={category}
+                        >
+                          <h1 className="font-bold mx-3 mt-3 pl-5">
+                            {category}
+                          </h1>
+                          <h1 className="font-bold mx-3 mt-3">Amount PHP</h1>
+                        </div>
+                        <hr className="mt-1 border h-[5px] bg-[#000000] ml-5"></hr>
+                        {Object.entries(payItems).map(([payItem, amount]) => {
+                          if (parseFloat(amount) != 0) {
+                            return (
+                              <>
+                                <div
+                                  className="flex flex-row justify-between"
+                                  key={payItem}
+                                >
+                                  <h1 className="mx-3 mt-3 pl-10">{payItem}</h1>
+                                  <h1 className="mx-3 mt-3">
+                                    {addCommaAndFormatDecimal(amount)}
+                                  </h1>
+                                </div>
+                              </>
+                            );
+                          }
+                        })}
+                        <hr className="mt-1 border h-[5px] bg-[#000000] ml-5"></hr>
+                        <div className="flex flex-row justify-between mb-5">
+                          <h1 className="font-bold mx-3 mt-3 pl-5">
+                            Total {category}
+                          </h1>
+                          <h1 className="mx-3 mt-3">
+                            {addCommaAndFormatDecimal(
+                              selectedRow["Totals"][category]
+                            )}
+                          </h1>
+                        </div>
+                        <hr className="mt-1 border h-[5px] bg-[#000000]"></hr>
+                      </>
+                    )
+                  )}
 
+                  <div className="flex flex-row justify-between border-t-3">
+                    <h1 className="font-bold mx-3 mt-3">Take Home Pay</h1>
+                    <h1 className="mx-3 mt-3">
+                      {addCommaAndFormatDecimal(selectedRow["Net Pay"])}
+                    </h1>
+                  </div>
+                  <hr className="mt-1 border h-[5px] bg-[#000000]"></hr>
+                </div>
+              </div>
+            </div>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog> */}
         <PreviewDialog data={selectedRow} />
       </div>
     </>
