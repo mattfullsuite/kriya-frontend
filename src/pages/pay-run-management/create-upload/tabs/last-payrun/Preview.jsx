a589142f51ae63603ce5a0937d1aa01025e0954f
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-06-14 14:23:24 +0800

    Update: Component Arguments & Parameters
    Minor update on arguments and parameters. Remove unused imports, codes, and console logs.

--- src/pages/pay-run-management/create-upload/tabs/last-payrun/Preview.jsx
+++ src/pages/pay-run-management/create-upload/tabs/last-payrun/Preview.jsx
@@ -3,55 +3,26 @@ import axios from "axios";
 import { PDFViewer } from "@react-pdf/renderer";
 import Payslip from "./react-pdf/Payslip.jsx";
 import { toast, ToastContainer } from "react-toastify";
-import { addCommaAndFormatDecimal } from "../../../assets/addCommaAndFormatDecimal.js";
-
 const Preview = ({
   payslipInformation,
   unprocessedPayables,
+  groupTotals,
   netBeforeTaxes,
   netPayEarnings,
 }) => {
-  console.log(
-    "Preview Dialog Parameters: ",
-    payslipInformation,
-    unprocessedPayables,
-    netBeforeTaxes,
-    netPayEarnings
-  );
   // Base URL for Axios
   const BASE_URL = process.env.REACT_APP_BASE_URL;
-  const companyInfoInitial = {
-    name: "Company Name",
-    logo: "",
-    tin: "TIN",
-    address: "Address",
-  };
-  const [companyInfo, setcompanyInfo] = useState(companyInfoInitial);
-  const [payslipInfo, setPaySlipInfo] = useState(payslipInformation);
-  const [selectedEmployeeData, setSelectedEmployeeData] = useState();
   const [groupTotal, setGroupTotal] = useState();
-  const [netPayBeforeTax, setNetPayBeforeTax] = useState({
-    lastPay: 0,
-    ytdGroup: 0,
-  });
-  const [netPayEarning, setNetPayEarning] = useState({
-    lastPay: 0,
-    ytdGroup: 0,
-  });
 
   useEffect(() => {
-    setPaySlipInfo(payslipInformation);
-    setSelectedEmployeeData(unprocessedPayables);
     calculateGroupTotal(unprocessedPayables);
-    setNetPayBeforeTax(netBeforeTaxes);
-    setNetPayEarning(netPayEarnings);
   }, [payslipInformation, unprocessedPayables, netBeforeTaxes, netPayEarnings]);
 
   const saveToDatabase = async () => {
     try {
       toast.promise(
         axios.post(BASE_URL + `/mp-createPayslip/${"Last Payrun"}`, [
-          payslipInfo,
+          payslipInformation,
         ]),
         {
           pending: "Saving Payslips...",
@@ -71,7 +42,6 @@ const Preview = ({
   };
 
   const calculateGroupTotal = (data) => {
-    const netPay = { lastPay: 0, ytdGroup: 0 };
     const groups = [...new Set(data.map((obj) => obj["pay_item_group"]))];
 
     const totals = groups.map((group) => {
@@ -87,8 +57,6 @@ const Preview = ({
           sum + parseFloat(item.last_pay_amount) + parseFloat(item.ytd_amount),
         0
       );
-      netPay.lastPay += lastPayGroup;
-      netPay.ytdGroup += ytdGroup;
       return {
         name: group,
         lastPay: lastPayGroup,
@@ -96,75 +64,7 @@ const Preview = ({
       };
     });
     setGroupTotal(totals);
-    // console.log("Group Totals: ", totals);
-    // setNetPayEarning(netPay);
-    // console.log("Net Pay: ", netPay);
-    // calculateNetBeforeTax(totals);
   };
-
-  const calculateNetBeforeTax = (data) => {
-    const netBeforeTax = { lastPay: 0, ytdGroup: 0 };
-    data.forEach((group) => {
-      if (
-        group.name == "Taxable" ||
-        group.name == "Non-Taxable" ||
-        group.name == "Pre-Tax Deduction"
-      ) {
-        netBeforeTax.lastPay += group.lastPay;
-        netBeforeTax.ytdGroup += group.lastPay;
-      }
-    });
-    setNetPayBeforeTax({
-      lastPay: netBeforeTax.lastPay,
-      ytdGroup: netBeforeTax.ytdGroup,
-    });
-    console.log("Net Before Totals: ", netBeforeTax);
-  };
-
-  // const renderRow = (groupName) => {
-  //   return (
-  //     groupTotal.length > 0 &&
-  //     groupTotal[groupName]?.lastPay !== 0 &&
-  //     groupTotal[groupName]?.ytdGroup !== 0 && (
-  //       <tbody>
-  //         {groupTotal
-  //           .filter((payItem) => payItem.name === groupName)
-  //           .map((item, index) => (
-  //             <tr key={index} className="bg-[#E6E7DD]">
-  //               <td className="font-bold w-1/2">{item.name}</td>
-  //               <td className="text-right w-1/4">
-  //                 {addCommaAndFormatDecimal(item.lastPay)}
-  //               </td>
-  //               <td className="text-right w-1/4">
-  //                 {addCommaAndFormatDecimal(item.ytdGroup)}
-  //               </td>
-  //             </tr>
-  //           ))}
-  //         {selectedEmployeeData.length > 0 &&
-  //           selectedEmployeeData
-  //             .filter(
-  //               (payItem) =>
-  //                 payItem.pay_item_group === groupName &&
-  //                 payItem.visible === true
-  //             )
-  //             .map((item, index) => (
-  //               <tr key={index}>
-  //                 <td>{item.pay_item_name}</td>
-  //                 <td className="text-right">
-  //                   {addCommaAndFormatDecimal(item.last_pay_amount)}
-  //                 </td>
-  //                 <td className="text-right">
-  //                   {addCommaAndFormatDecimal(
-  //                     parseFloat(item.last_pay_amount) +
-  //                       parseFloat(item.ytd_amount)
-  //                   )}
-  //                 </td>
-  //               </tr>
-  //             ))}
-  //       </tbody>
-  //     )
-  //   );
-  // };
 
   return (
     <>
@@ -197,137 +97,13 @@ const Preview = ({
 
         <PDFViewer style={{ width: "80vw", height: "90vh" }}>
           <Payslip
-            payslipInformation={payslipInfo}
+            payslipInformation={payslipInformation}
             unprocessedPayables={unprocessedPayables}
-            groupTotals={groupTotal}
-            netBeforeTaxes={netPayBeforeTax}
-            netPayEarnings={netPayEarning}
+            groupTotals={groupTotals}
+            netBeforeTaxes={netBeforeTaxes}
+            netPayEarnings={netPayEarnings}
           />
         </PDFViewer>
-
-        {/* {selectedEmployeeData && (
-          <div className="p-2 w-2/3 overflow-x-auto bg-white">
-            <table className="table">
-              <thead>
-                <tr className="bg-[#666A40] text-white">
-                  <th>Last Payrun Calculation</th>
-                  <th>Last Pay</th>
-                  <th>Total Earnings</th>
-                </tr>
-              </thead>
-              {renderRow("Taxable")}
-              {renderRow("Non-Taxable")}
-              {renderRow("Pre-Tax Deduction")}
-
-              <tbody>
-                <tr className="bg-[#666A40] text-white font-bold">
-                  <td className="font-bold w-1/2">
-                    Net Pay Before Tax Deduction
-                  </td>
-                  <td className="text-right w-1/4">
-                    {addCommaAndFormatDecimal(netPayBeforeTax.lastPay)}
-                  </td>
-                  <td className="text-right w-1/4">
-                    {addCommaAndFormatDecimal(netPayBeforeTax.ytdGroup)}
-                  </td>
-                </tr>
-              </tbody>
-              <tbody>
-                <tr className="bg-[#E6E7DD]">
-                  <td className="font-bold" colSpan="3">
-                    Taxes
-                  </td>
-                </tr>
-                <tr className="">
-                  <td className="font-bold w-1/2">
-                    Withheld Taxes During The Year
-                  </td>
-                  <td className="text-right w-1/4"></td>
-                  <td className="text-right w-1/4">
-                    {selectedEmployeeData.length > 0 &&
-                      selectedEmployeeData
-                        .filter(
-                          (payItem) => payItem.pay_item_name === "Tax Withheld"
-                        )
-                        .map((payItem, index) => (
-                          <div key={index}>
-                            {addCommaAndFormatDecimal(
-                              Math.abs(payItem.ytd_amount)
-                            )}
-                          </div>
-                        ))}
-                  </td>
-                </tr>
-                <tr className="">
-                  <td className="font-bold w-1/2">Tax Due</td>
-                  <td className="text-right w-1/4"></td>
-                  <td className="text-right w-1/4">
-                    {selectedEmployeeData.length > 0 &&
-                      selectedEmployeeData
-                        .filter(
-                          (payItem) => payItem.pay_item_name === "Tax Withheld"
-                        )
-                        .map((payItem, index) => (
-                          <div key={index}>
-                            {addCommaAndFormatDecimal(
-                              Math.abs(payItem.last_pay_amount)
-                            )}
-                          </div>
-                        ))}
-                  </td>
-                </tr>
-                <tr className="">
-                  <td className="font-bold w-1/2">Tax Refund (Tax Payable)</td>
-                  <td className="text-right w-1/4">
-                    {selectedEmployeeData.length > 0 &&
-                      selectedEmployeeData
-                        .filter(
-                          (payItem) => payItem.pay_item_name === "Tax Withheld"
-                        )
-                        .map((payItem, index) => (
-                          <div key={index}>
-                            {addCommaAndFormatDecimal(
-                              Math.abs(payItem.ytd_amount) -
-                                Math.abs(payItem.last_pay_amount)
-                            )}
-                          </div>
-                        ))}
-                  </td>
-                  <td className="text-right w-1/4">
-                    {selectedEmployeeData.length > 0 &&
-                      selectedEmployeeData
-                        .filter(
-                          (payItem) =>
-                            payItem.pay_item_name ===
-                            "Tax Refund - Current Year"
-                        )
-                        .map((payItem, index) => (
-                          <div key={index}>
-                            {addCommaAndFormatDecimal(
-                              Math.abs(payItem.last_pay_amount)
-                            )}
-                          </div>
-                        ))}
-                  </td>
-                </tr>
-              </tbody>
-              {renderRow("Post-Tax Deduction")}
-              {renderRow("Post-Tax Addition")}
-
-              <tbody>
-                <tr className="bg-[#666A40] text-white font-bold">
-                  <td className="font-bold">NET PAY EARNINGS</td>
-                  <td className="text-right w-1/4">
-                    {addCommaAndFormatDecimal(netPayEarning.lastPay)}
-                  </td>
-                  <td className="text-right w-1/4">
-                    {addCommaAndFormatDecimal(netPayEarning.ytdGroup)}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        )} */}
 
         <div className="flex flex-row gap-2 my-2 p-2 w-4/5 justify-end">
           <button className="btn" onClick={() => saveToDatabase()}>
