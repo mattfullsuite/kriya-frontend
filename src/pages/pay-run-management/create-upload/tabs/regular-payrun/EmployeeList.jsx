43c6b20f550e7c19ed2b17cf1b75e19b3ef077ba
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-07-11 15:18:02 +0800

    Initial Regular Payrun

---
+++ src/pages/pay-run-management/create-upload/tabs/regular-payrun/EmployeeList.jsx
@@ -0,0 +1,18 @@
+const EmployeeList = () => {
+  return (
+    <>
+      <div className="mt-5 flex flex-col border-2  border-[#E4E4E4] rounded-[15px] p-5 bg-white">
+        <table>
+          <thead>
+            <th>Employee Name</th>
+          </thead>
+          <tr>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default EmployeeList;
