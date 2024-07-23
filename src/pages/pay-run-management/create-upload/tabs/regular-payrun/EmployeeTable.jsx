f502519452e4b9d50cfad86b864e5f812e08fce9
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-07-23 15:30:07 +0800

    Update: Dropdown Menu
    Fixed Issue with adding new pay items.

--- src/pages/pay-run-management/create-upload/tabs/regular-payrun/EmployeeTable.jsx
+++ src/pages/pay-run-management/create-upload/tabs/regular-payrun/EmployeeTable.jsx
@@ -55,7 +55,6 @@ const EmployeeTable = ({ employeeList, payItems }) => {
   const handleColumnAddition = (columnName) => {
     setVisibleColumns((prev) => [...prev, columnName]);
     setHiddenColumns((prev) => prev.filter((col) => col !== columnName));
-    setDropdownVisible(false);
   };
 
   return (
@@ -64,39 +63,24 @@ const EmployeeTable = ({ employeeList, payItems }) => {
         <div className="mt-5 flex flex-col border-2 border-[#E4E4E4] rounded-[15px] p-5 bg-white overflow-auto">
           <table className="h-96">
             <thead>
-              <tr className="whitespace-nowrap text-left align-top border-b-4">
+              <tr className=" text-left align-top border-b-4">
                 {visibleColumns
                   .filter((key) => !hiddenColumns.includes(key))
                   .map((key) => (
-                    <th className="px-2" key={key}>
+                    <th className="px-2 h-20 w-36" key={key}>
                       {key}
                     </th>
                   ))}
-                <th className="px-2">
-                  <button onClick={toggleDropdown}>+</button>
-                  {dropdownVisible && (
-                    <ul className="absolute bg-white shadow-md">
-                      {hiddenColumns.map((col) => (
-                        <li
-                          key={col}
-                          onClick={() => handleColumnAddition(col)}
-                          className="cursor-pointer p-2 hover:bg-gray-200"
-                        >
-                          {col}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </th>
+                <th className="px-2 h-20 w-40">Add Pay Item</th>
               </tr>
             </thead>
             <tbody>
               {empList.map((employee, index) => (
-                <tr key={index}>
+                <tr className="border-b px-4 whitespace-nowrap" key={index}>
                   {visibleColumns
                     .filter((key) => !hiddenColumns.includes(key))
                     .map((key) => (
-                      <td key={key} className="border-b px-2 whitespace-nowrap">
+                      <td key={key}>
                         {isNaN(Number(employee[key])) ? (
                           employee[key]
                         ) : (
@@ -109,6 +93,20 @@ const EmployeeTable = ({ employeeList, payItems }) => {
                         )}
                       </td>
                     ))}
+                  <td>
+                    <select
+                      onChange={(e) => handleColumnAddition(e.target.value)}
+                      className="cursor-pointer p-2 hover:bg-gray-200"
+                    >
+                      <option>Select an Item</option>
+                      {hiddenColumns &&
+                        hiddenColumns.map((col) => (
+                          <option key={col} value={col}>
+                            {col}
+                          </option>
+                        ))}
+                    </select>
+                  </td>
                 </tr>
               ))}
             </tbody>
