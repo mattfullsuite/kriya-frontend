9d76e95f2371cac647e761376653e65889aa5af5
Matt Wilfred Salvador <smattwilfred01@gmail.com>
2024-07-30 11:14:00 +0800

    slack api integration for overtime and ptos and cheer apeerx

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -4,10 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import UserLogs from "../pages/admin/UserLogs";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import AdminPortal from "../pages/AdminPortal";
+import AdminEmployee from "../Layout/AdminEmployee";
 
 const AdminRoutes = () => {
   return (
     <Routes>
+      <Route path="/admin" element={<AdminEmployee />}/>
       <Route path="/userLogs" element={<UserLogs />} />
       <Route path="/adminDashboard" element={<AdminDashboard />} />
       <Route path="/ts-admin" element={<AdminPortal />} />
