224761780ed257c835030667e0fedaf2289ddf9e
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-08-02 08:07:34 +0800

    Add: Button
    Added next buttor for next step.

--- src/pages/pay-run-management/create-upload/tabs/regular-payrun/EmployeeTable.jsx
+++ src/pages/pay-run-management/create-upload/tabs/regular-payrun/EmployeeTable.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { NewInput } from "./NewInput";
 
-const EmployeeTable = ({ employeeList, payItems }) => {
-  const [empList, setEmpList] = useState(null);
+const EmployeeTable = ({
+  employeeList,
+  setEmployeeList,
+  payItems,
+  nextClick,
+}) => {
   const [payItemsList, setPayItemsList] = useState(null);
   const [payItemsTypes, setPayItemsTypes] = useState(null);
   const [hiddenColumns, setHiddenColumns] = useState([]);
@@ -10,7 +14,7 @@ const EmployeeTable = ({ employeeList, payItems }) => {
 
   useEffect(() => {
     if (employeeList && payItems) {
-      setEmpList(employeeList);
+      document.getElementById("step-2").style.display = "block";
       getTypes(payItems);
       setPayItemsList(payItems);
       columnsToHide(employeeList, payItems);
@@ -18,7 +22,7 @@ const EmployeeTable = ({ employeeList, payItems }) => {
   }, [employeeList, payItems]);
 
   const handleInput = (dataIndex, payItem, value) => {
-    setEmpList((prevList) => {
+    setEmployeeList((prevList) => {
       const updatedList = [...prevList];
       updatedList[dataIndex] = {
         ...updatedList[dataIndex],
@@ -63,59 +67,73 @@ const EmployeeTable = ({ employeeList, payItems }) => {
 
   return (
     <>
-      {empList && (
-        <div className="mt-5 flex flex-col border-2 border-[#E4E4E4] rounded-[15px] p-5 bg-white overflow-auto">
-          <table className="h-96">
-            <thead>
-              <tr className=" text-left align-top border-b-4">
-                {visibleColumns
-                  .filter((key) => !hiddenColumns.includes(key))
-                  .map((key) => (
-                    <th className="px-2 h-20 w-36" key={key}>
-                      {key}
-                    </th>
-                  ))}
-                <th className="px-2 h-20 w-40">Add Pay Item</th>
-              </tr>
-            </thead>
-            <tbody>
-              {empList.map((employee, index) => (
-                <tr className="border-b px-4 whitespace-nowrap" key={index}>
-                  {visibleColumns
-                    .filter((key) => !hiddenColumns.includes(key))
-                    .map((key) => (
-                      <td key={key}>
-                        {isNaN(Number(employee[key])) ? (
-                          employee[key]
-                        ) : (
-                          <NewInput
-                            dataIndex={index}
-                            dataKey={key}
-                            data={employee[key]}
-                            onValueChange={handleInput}
-                          />
-                        )}
-                      </td>
-                    ))}
-                  <td>
-                    <select
-                      onChange={(e) => handleColumnAddition(e.target.value)}
-                      className="cursor-pointer p-2 hover:bg-gray-200"
-                    >
-                      <option>Select an Item</option>
-                      {hiddenColumns &&
-                        hiddenColumns.map((col) => (
-                          <option key={col} value={col}>
-                            {col}
-                          </option>
+      {employeeList && (
+        <>
+          <div id="step-2">
+            <div className="mt-5 flex flex-col border-2 border-[#E4E4E4] rounded-[15px] p-5 bg-white overflow-auto">
+              <table className="h-96">
+                <thead>
+                  <tr className=" text-left align-top border-b-4">
+                    {visibleColumns
+                      .filter((key) => !hiddenColumns.includes(key))
+                      .map((key) => (
+                        <th className="px-2 h-20 w-36" key={key}>
+                          {key}
+                        </th>
+                      ))}
+                    <th className="px-2 h-20 w-40">Add Pay Item</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {employeeList.map((employee, index) => (
+                    <tr className="border-b px-4 whitespace-nowrap" key={index}>
+                      {visibleColumns
+                        .filter((key) => !hiddenColumns.includes(key))
+                        .map((key) => (
+                          <td key={key}>
+                            {isNaN(Number(employee[key])) ? (
+                              employee[key]
+                            ) : (
+                              <NewInput
+                                dataIndex={index}
+                                dataKey={key}
+                                data={employee[key]}
+                                onValueChange={handleInput}
+                              />
+                            )}
+                          </td>
                         ))}
-                    </select>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                      <td>
+                        <select
+                          onChange={(e) => handleColumnAddition(e.target.value)}
+                          className="cursor-pointer p-2 hover:bg-gray-200"
+                        >
+                          <option>+ Add Pay Item</option>
+                          {hiddenColumns &&
+                            hiddenColumns.map((col) => (
+                              <option key={col} value={col}>
+                                {col}
+                              </option>
+                            ))}
+                        </select>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="p-5 w-full flex">
+              <button
+                type="button"
+                className="btn bg-[#666A40] shadow-md w-32 text-white hover:bg-[#666A40] hover:opacity-80 ml-auto "
+                onClick={nextClick}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
