d46e966804d5221ee0078fd262c1a12c64b6df71
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-10-16 11:27:02 +0800

    Add: Column
    Added column for add notes button.

--- src/pages/pay-run-management/create-upload/tabs/regular-payrun/Step3.jsx
+++ src/pages/pay-run-management/create-upload/tabs/regular-payrun/Step3.jsx
@@ -11,6 +11,7 @@ const Step3 = ({
   finalizeClick,
   payItems,
   draft,
+  displayAddNotes,
 }) => {
   const [employeeList, setEmployeeList] = useState();
   const [visibleColumns, setVisibleColumns] = useState([]);
@@ -52,7 +53,8 @@ const Step3 = ({
       "Net Pay (PP-3)",
       "Filed PTO Days",
       "Total Absences",
-      "Unpaid Leaves"
+      "Unpaid Leaves",
+      "Notes"
     );
     setVisibleColumns(visibleCols);
   };
@@ -96,6 +98,10 @@ const Step3 = ({
     // Trigger the download and remove the link
     link.click();
     document.body.removeChild(link);
+  };
+
+  const onDisplayNotesClick = (e) => {
+    displayAddNotes(e.target.id, employeeList);
   };
 
   return (
@@ -142,13 +148,39 @@ const Step3 = ({
                 <tbody>
                   {employeeList?.map((employee, index) => (
                     <tr className="border-b" key={index}>
-                      {visibleColumns.map((key) => (
-                        <td key={key} className="p-2">
-                          {isNaN(employee[key])
-                            ? employee[key]
-                            : addComma(formatDecimal(employee[key].toString()))}
-                        </td>
-                      ))}
+                      {visibleColumns.map(
+                        (key) =>
+                          key != "Notes" && (
+                            <td key={key} className="p-2">
+                              {isNaN(employee[key])
+                                ? employee[key]
+                                : addComma(
+                                    formatDecimal(employee[key].toString())
+                                  )}
+                            </td>
+                          )
+                      )}
+                      <td>
+                        <button
+                          id={employee["Employee ID"]}
+                          className="btn bg-[#666A40] shadow-md w-12 text-white hover:bg-[#666A40] hover:opacity-80"
+                          onClick={(e) => onDisplayNotesClick(e)}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="size-6"
+                          >
+                            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                            <path
+                              fill-rule="evenodd"
+                              d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                              clip-rule="evenodd"
+                            />
+                          </svg>
+                        </button>
+                      </td>
                     </tr>
                   ))}
                 </tbody>
