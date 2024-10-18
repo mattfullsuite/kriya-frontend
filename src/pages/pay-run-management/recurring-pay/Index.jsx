a1721e4fd3693ca38de13559b197317af171f9d5
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-10-18 09:21:52 +0800

    Import: Component
    Imported component of add recurring pay form.

--- src/pages/pay-run-management/recurring-pay/Index.jsx
+++ src/pages/pay-run-management/recurring-pay/Index.jsx
@@ -3,17 +3,29 @@ import { getRecords } from "./AxiosFunctions";
 
 import Headings from "../../../components/universal/Headings";
 import RecurringPayRecords from "./RecurringPayRecords";
+import AddRecurringPay from "./AddRecurringPay";
 
 const RecurringPay = () => {
+  const [recurringPayList, setRecurringPayList] = useState();
+
   useEffect(async () => {
     console.log(await getRecords());
+    setRecurringPayList("");
   }, []);
+
+  const showAddForm = () => {
+    document.getElementById(`dialog-add`).showModal();
+  };
 
   return (
     <>
       <div className="p-5 min-w-[320px] max-w-[1300px]">
         <Headings text={"Recurring Pay Items"} />
-        <RecurringPayRecords />
+        <RecurringPayRecords
+          recurringPayList={recurringPayList}
+          showAddForm={showAddForm}
+        />
+        <AddRecurringPay />
       </div>
     </>
   );
