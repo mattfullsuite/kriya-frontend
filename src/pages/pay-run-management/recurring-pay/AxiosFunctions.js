83ef4d99246e2eea6203c4b3f78f6ea7b0c59437
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-10-19 09:42:47 +0800

    Update: Function
    Updated function of get records.

--- src/pages/pay-run-management/recurring-pay/AxiosFunctions.js
+++ src/pages/pay-run-management/recurring-pay/AxiosFunctions.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const getRecords = async () => {
+
+export const getAllRecords = async () => {
+  try {
+    const result = await axios.get(BASE_URL + `/rp-GetAllRecurringPay`);
+    if (result.data.length > 0) {
+      return result.data;
+    }
+    return [];
+  } catch (error) {
+    return "ERROR: " + error;
+  }
+};
+
+export const getCertainRecord = async () => {
   try {
     const result = await axios.get(BASE_URL + ``);
     if (result.data.length > 0) {
