23b7dda32bf8292dae2b408a3bccc34d3cac9bbd
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-10-22 10:01:27 +0800

    Itegration: Functions
    Integrated Functions for getting employee and pay items.

--- src/pages/pay-run-management/recurring-pay/Index.jsx
+++ src/pages/pay-run-management/recurring-pay/Index.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
-import { getAllRecords } from "./AxiosFunctions";
+import {
+  createRecord,
+  getAllRecords,
+  getEmployeeList,
+  getPayItemList,
+} from "./AxiosFunctions";
 
 import Headings from "../../../components/universal/Headings";
 import RecurringPayRecords from "./RecurringPayRecords";
@@ -8,9 +13,13 @@ import EditDialog from "./DialogEdit";
 
 const RecurringPay = () => {
   const [recurringPayList, setRecurringPayList] = useState([]);
+  const [employeeList, setEmployeeList] = useState([]);
+  const [payItemList, setPayItemList] = useState([]);
 
   useEffect(() => {
     retrieveAllrecords();
+    getEmployeeRecords();
+    getPayItemRecords();
   }, []);
 
   const retrieveAllrecords = async () => {
@@ -28,6 +37,20 @@ const RecurringPay = () => {
     document.getElementById(`dialog-edit`).showModal();
   };
 
+  const getEmployeeRecords = async () => {
+    const retrievedList = await getEmployeeList();
+    if (retrievedList.length > 0) {
+      setEmployeeList(retrievedList);
+    }
+  };
+
+  const getPayItemRecords = async () => {
+    const retrievedList = await getPayItemList();
+    if (retrievedList.length > 0) {
+      setPayItemList(retrievedList);
+    }
+  };
+
   return (
     <>
       <div className="p-5 min-w-[320px] max-w-[1300px]">
@@ -37,7 +60,11 @@ const RecurringPay = () => {
           showAddForm={showAddForm}
           showEditRecord={showEditRecord}
         />
-        <AddDialog createRecord={createRecord} />
+        <AddDialog
+          createRecord={createRecord}
+          employeeList={employeeList}
+          payItemList={payItemList}
+        />
         <EditDialog />
       </div>
     </>
