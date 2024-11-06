8e4128dbadd30e6a9d9c459ffba96d5c78b7b371
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-11-06 11:04:22 +0800

    Create: Function
    Created function for getting employee info for payroll notif.

--- src/pages/pay-run-management/create-upload/tabs/regular-payrun/AxiosFunctions.js
+++ src/pages/pay-run-management/create-upload/tabs/regular-payrun/AxiosFunctions.js
@@ -2,3 +2,17 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const GetPayrollNotifRecordInfo = async (empID) => {
+  try {
+    const result = await axios.get(
+      BASE_URL + `/rp-GetPayrollNotifRecordInfo/${empID}`
+    );
+    if (result.data.length > 0) {
+      return result.data;
+    }
+    return [];
+  } catch (error) {
+    return "ERROR: " + error;
+  }
+};
