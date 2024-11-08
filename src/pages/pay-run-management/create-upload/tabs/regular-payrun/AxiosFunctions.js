d273813f5cab57103980f32eb3a8017072c12d5b
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-11-08 10:09:47 +0800

    Fix: URL

--- src/pages/pay-run-management/create-upload/tabs/regular-payrun/AxiosFunctions.js
+++ src/pages/pay-run-management/create-upload/tabs/regular-payrun/AxiosFunctions.js
@@ -6,7 +6,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const GetPayrollNotifRecordInfo = async (empID) => {
   try {
     const result = await axios.get(
-      BASE_URL + `/rp-GetPayrollNotifRecordInfo/${empID}`
+      BASE_URL + `/mp-cu-GetPayrollNotifRecordInfo/${empID}`
     );
     if (result.data.length > 0) {
       return result.data;
